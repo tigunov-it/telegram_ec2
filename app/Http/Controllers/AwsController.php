75b9955ac54c2917ebfb057565fb2ca09fe022c7
Yuriy Tigunov <192215@gmail.com>
2022-06-23 11:55:25 +0800

    Initial commit

---
+++ app/Http/Controllers/AwsController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Telegram;
+use Aws\Ec2\Ec2Client;
+use Illuminate\Http\Request;
+
+class AwsController extends Controller
+{
+    public function startInstance(Request $request, Telegram $telegram){
+
+        $ec2Client = new Ec2Client([
+            'region' => env('AWS_DEFAULT_REGION'),
+            'version' => 'latest'
+        ]);
+
+        $action = $request->input('message')['text'];
+
+//        $instanceIds = array('InstanceID1', 'InstanceID2');
+        $instanceIds = array(env('AWS_EC2_INSTANCES'));
+
+        if ($action == 'start') {
+            $result = $ec2Client->startInstances(array(
+                'InstanceIds' => $instanceIds,
+            ));
+            $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'instance starting');
+        } else {
+            $result = $ec2Client->stopInstances(array(
+                'InstanceIds' => $instanceIds,
+            ));
+            $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'instance stopping');
+        }
+
+    }
+}
---
+++ app/Http/Controllers/WebhookController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\Telegram;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+
+class WebhookController extends Controller
+{
+    public function index(Request $request, Telegram $telegram)
+    {
+        Log::debug($request->input('message')['text']);
+//        $telegram->sendMessage(env('TELEGRAM_CHAT_ID'), 'Pong');
+    }
+}
